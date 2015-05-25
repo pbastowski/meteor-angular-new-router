c6947da028923e37f5f08aea0b1dd48d8f29d4d4
Paul Bastowski <pbastowski@gmail.com>
2015-05-25 18:56:15 +0200

    fixed up readme and bower.json

---
+++ README.md
@@ -0,0 +1,11 @@
+## The new angular 1.4 router Meteor package
+
+Work in progress...
+
+To develop type 
+
+```bash
+   bower install
+```
+
+in the package folder after cloning.
---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'pbastowski:angular-new-router',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Packaged up angular-new-router directly from bower',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/angular/router',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('./bower_components/angular-new-router/dist/router.es5.min.js', ['client']);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('pbastowski:angular-new-router');
+  api.addFiles('angular-new-router-tests.js');
+});
